b08af849597a755ab2179c77cb4d1873c44f0ae6
Luvest <slavokugfk@gmail.com>
2021-01-21 14:29:04 +0500

    deleted the excess

--- db/migrate/20210117213841_create_questions.rb
+++ db/migrate/20210117213841_create_questions.rb
@@ -2,7 +2,6 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
       t.text :body
-      t.integer :test_id
       t.references :test, foreign_key: true
 
       t.timestamps
--- db/migrate/20210117214322_create_answers.rb
+++ db/migrate/20210117214322_create_answers.rb
@@ -3,7 +3,6 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
     create_table :answers do |t|
       t.string :body
       t.boolean :correct
-      t.integer :question_id
       t.references :question, foreign_key: true
 
       t.timestamps
