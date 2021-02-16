cc6c86804a34dea55bb5ef75958e92ab9d086242
Luvest <slavokugfk@gmail.com>
2021-02-17 00:38:55 +0300

    Authentication, user sessions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to "Author: #{author_name}", "https://github.com/#{author_name}/#{repo_name}", target: :blank
   end
+
+  def flash_message(key)
+    if flash[key]
+      content_tag :p, flash[key], class: 'flash message'
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_103523) do
+ActiveRecord::Schema.define(version: 2021_02_16_212851) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2021_02_14_103523) do
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "tests", "users", column: "author_id"
