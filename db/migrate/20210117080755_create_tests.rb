8d50d39e18c977260f24bceec0636ad79a4fcb70
Luvest <slavokugfk@gmail.com>
2021-03-07 22:32:41 +0400

    DB fix category

--- db/migrate/20210117080755_create_tests.rb
+++ db/migrate/20210117080755_create_tests.rb
@@ -3,8 +3,6 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :title
       t.integer :level
-      t.references :category, foreign_key: true
-
 
       t.timestamps
     end
